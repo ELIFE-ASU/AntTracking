7cfdc018fdad57830c57eef75defb1ff7d62553f
Siyu Zhou <--global>
2017-11-12 12:42:38 -0700

    Model trained with normalization.

--- src/cv2test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread('ant_img/tandem/0.png', 0)
-ret, th = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-plt.imshow(th, 'gray')
-plt.show()
